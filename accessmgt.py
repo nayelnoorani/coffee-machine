a00deaac1c3f384579fe0c96b95c140e7c39d028
Nayel Noorani <nayel.noorani@gmail.com>
2024-06-27 18:54:26 +0500

    Update accessmgt.py
    cleaned up code pattern if (condition) return true else false to return (condition)

--- accessmgt.py
+++ accessmgt.py
@@ -2,26 +2,15 @@ from users import users
 import getpass
 
 def user_exists(username):
-    if username in users:
-        return True
-    else:
-        return False
+    return username in users
     
 def check_password(username, password):
     if user_exists(username):
-        if users[username]['password'] == password:
-            return True
-        else:
-            return False
-    else:
-        return False
+        return users[username]['password'] == password 
 
 def authenticate_user():
     username = input("Username: ")
     password = getpass.getpass("Password: ")
-    if (user_exists(username) 
+    return (user_exists(username) 
         and check_password(username, password)
-        and users[username]['role'] == 'admin'):
-        return True
-    else:
-        return False
+        and users[username]['role'] == 'admin')
